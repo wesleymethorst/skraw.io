9d69f9410a474453e62b0036f32e8f57542396ad
wesleymethorst <wesley.methorst@gmail.com>
2025-05-27 14:00:54 +0200

    Fix typo in AI guess box label

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
       </div>
       <div class="right-panel">
         <div class="chat-box">Text Box</div>
-        <div class="ai-box">AI guessr</div>
+        <div class="ai-box">AI guess</div>
       </div>
     </div>
   </div>
