9f9ab111ab3128c6de973a5442a54b3f36e992df
wesleymethorst <wesley.methorst@gmail.com>
2025-06-05 14:28:24 +0200

    Refactor socket configuration in frontend socket.js to improve connection settings by enabling autoConnect and adjusting transport order for better performance.

--- frontend/src/config/socket.js
+++ frontend/src/config/socket.js
@@ -3,12 +3,10 @@ import { io } from 'socket.io-client'
 // Backend URL configuratie
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'
 
-console.log(BACKEND_URL)
-
 // Socket.IO instance
 export const socket = io(BACKEND_URL, {
-  transports: ['polling', 'websocket'],
-  withCredentials: true
+  transports: ['websocket', 'polling'],
+  autoConnect: true
 })
 
 export default socket 
